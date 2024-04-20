575a1f8dac25f8edbd275cd40557d8e1d49d8845
Tuan Minh <gtuanminh1007@gmail.com>
2024-04-20 20:08:00 +0700

    add proficiency section

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import dynamic from "next/dynamic";
 const NavigationBar = dynamic(() => import('@/components/NavigationBar'));
 const Greetings = dynamic(() => import('@/containers/Greetings'));
 const Skills = dynamic(() => import('@/containers/Skills'));
+const Proficiency = dynamic(() => import('@/containers/Proficiency'));
 import SEO from "@/components/SEO";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
       <NavigationBar />
       <Greetings />
       <Skills />
+      <Proficiency />
     </div>
   );
 }
